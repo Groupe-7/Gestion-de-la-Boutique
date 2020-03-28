a660ba87d009120226c992898ca5ffb6805afaed
Yinra2010 <62189377+Yinra2010@users.noreply.github.com>
2020-03-28 10:23:36 +0100

    Update and rename Connexion.php to login.php

---
+++ login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Login page</title>
+		
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<div id= "frm">
+				<form action="process.php" method="POST"> 
+				<p>
+				<label>Username</label> 
+				<input type="texte" id="user" name="user"/>
+				
+				</p>
+				
+				<p>
+				<label>Password</label> 
+				<input type="password" id="pass" name="pass"/>
+				
+				</p>
+				
+				<p>
+				<input type="submit" id="btn" value="Login"/>
+				
+				</p>
+				
+					
+				</form>
+				
+			</div>
+		
+	</body>
+</html>

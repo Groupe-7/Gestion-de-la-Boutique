56a93c693e9422fb58b387a294d5e2f4703f5c8a
Yinra2010 <62189377+Yinra2010@users.noreply.github.com>
2020-03-28 10:24:49 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!Doctype html>
+	<html>
+		<head>
+			<title>Connecting to a Database</title>		
+		</head>
+		<body>
+			<?php
+				$con = mysql_connect ('localhost','root','');
+				$db = mysql_select_db('my_database');
+				
+				if($con){
+					echo 'Connecté à la BD';
+				}
+				else {
+					die('erreur.');
+				}
+				if ($db){
+					echo 'Base de données trouvée';
+				}
+				else {
+					die ('erreur. Base de donnée non trouvée');
+				}
+			?>
+			
+			<br/>
+			<br/>
+			
+			<?php
+				$query = mysql_query("select * FROM data");
+				while($row = mysql_fetch_array($query)){
+					$id = $row['id'];
+					$name = $row['name'];
+					echo $id . ': ' .$name . '<br/>';
+				}
+			?>
+			
+			
+		</body>
+
+	</html>
